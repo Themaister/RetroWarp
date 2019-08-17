26598a82dbe04e8422d9c4ed3060b0121ee17a8d
Hans-Kristian Arntzen <maister@archlinux.us>
2019-08-17 14:21:17 +0200

    Lerp fractional table.

--- approximate_divider.cpp
+++ approximate_divider.cpp
@@ -15,23 +15,26 @@ static inline uint32_t leading_zeroes(uint32_t x)
 #error "Implement me."
 #endif
 
-enum { INVERSE_BITS = 10 };
-static int32_t inverse_table[1 << INVERSE_BITS];
+enum { INVERSE_BITS = 4 };
+static int32_t inverse_table[1 << INVERSE_BITS + 1];
 
 void setup_fixed_divider()
 {
-	for (unsigned i = 0; i < 1 << INVERSE_BITS; i++)
-		inverse_table[i] = int32_t(double(-0x40000000) * 1.0 / (0.5 + (0.5 / (1 << INVERSE_BITS)) * double(i)));
+	for (unsigned i = 0; i <= 1 << INVERSE_BITS; i++)
+		inverse_table[i] = int32_t(double(-0x400000) * 1.0 / (0.5 + (0.5 / (1 << INVERSE_BITS)) * double(i)));
 }
 
 int32_t fixed_divider(int32_t x, uint32_t y, unsigned extra_bits)
 {
 	unsigned leading = leading_zeroes(y);
 	y <<= leading;
-	y >>= (31 - INVERSE_BITS);
+	y >>= (31 - INVERSE_BITS - 8);
+
+	int rcp_frac = y & 0xff;
+	y >>= 8;
 	y &= (1 << INVERSE_BITS) - 1;
 
-	int64_t rcp = inverse_table[y];
+	int64_t rcp = inverse_table[y] * (0x100 - rcp_frac) + inverse_table[y + 1] * rcp_frac;
 	int32_t res = -int32_t((int64_t(x) * rcp) >> (30 - extra_bits));
 
 	unsigned msb_index = 32 - leading;
