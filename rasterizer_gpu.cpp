132c2b086a2888bc31d5d39d6f60c8d57c1683b0
Hans-Kristian Arntzen <maister@archlinux.us>
2019-08-20 12:05:05 +0200

    Coarse rasterization.

--- rasterizer_gpu.cpp
+++ rasterizer_gpu.cpp
@@ -22,6 +22,7 @@ struct RasterizerGPU::Impl
 	unsigned height = 0;
 
 	BufferHandle binning_mask_buffer;
+	BufferHandle binning_mask_buffer_coarse;
 };
 
 struct Registers
@@ -56,6 +57,8 @@ RasterizerGPU::RasterizerGPU()
 	info.usage = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
 	info.size = ((MAX_WIDTH + 15) / 16) * ((MAX_HEIGHT + 15) / 16) * (MAX_PRIMITIVES / 8);
 	impl->binning_mask_buffer = impl->device.create_buffer(info);
+	info.size = (info.size + 31) / 32;
+	impl->binning_mask_buffer_coarse = impl->device.create_buffer(info);
 }
 
 RasterizerGPU::~RasterizerGPU()
@@ -143,26 +146,39 @@ void RasterizerGPU::rasterize_primitives(const RetroWarp::PrimitiveSetup *setup,
 	reg.scissor_y = 0;
 	reg.scissor_width = impl->width;
 	reg.scissor_height = impl->height;
-	cmd->push_constants(&reg, 0, sizeof(reg));
 
 	// Binning
 	cmd->set_program("assets://shaders/binning.comp");
 	cmd->set_storage_buffer(0, 0, *impl->binning_mask_buffer);
 	cmd->set_storage_buffer(0, 1, *primitive_buffer_pos);
+	cmd->push_constants(&reg, 0, sizeof(reg));
 	cmd->dispatch((count + 63) / 64, (impl->width + 15) / 16, (impl->height + 15) / 16);
 
 	cmd->barrier(VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_ACCESS_SHADER_WRITE_BIT,
 	             VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_ACCESS_SHADER_READ_BIT);
 
+	// Merge coarse mask
+	cmd->set_program("assets://shaders/build_coarse_mask.comp");
+	cmd->set_storage_buffer(0, 0, *impl->binning_mask_buffer);
+	cmd->set_storage_buffer(0, 1, *impl->binning_mask_buffer_coarse);
+	uint32_t num_masks = (count + 31) / 32;
+	cmd->push_constants(&num_masks, 0, sizeof(num_masks));
+	cmd->dispatch((num_masks + 63) / 64, (impl->width + 15) / 16, (impl->height + 15) / 16);
+
+	cmd->barrier(VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_ACCESS_SHADER_WRITE_BIT,
+	             VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_ACCESS_SHADER_READ_BIT);
+
 	// Rasterization
+	cmd->set_program("assets://shaders/rasterize.comp");
 	cmd->set_storage_buffer(0, 0, *primitive_buffer_pos);
 	cmd->set_storage_buffer(0, 1, *primitive_buffer_attr);
 	cmd->set_storage_buffer(0, 2, *impl->color_buffer);
 	cmd->set_storage_buffer(0, 3, *impl->depth_buffer);
 	cmd->set_storage_buffer(0, 4, *impl->binning_mask_buffer);
+	cmd->set_storage_buffer(0, 5, *impl->binning_mask_buffer_coarse);
 	cmd->set_texture(1, 0, impl->image->get_view());
 
-	cmd->set_program("assets://shaders/rasterize.comp");
+	cmd->push_constants(&reg, 0, sizeof(reg));
 	cmd->dispatch((impl->width + 15) / 16, (impl->height + 15) / 16, 1);
 	impl->device.submit(cmd);
 	impl->device.next_frame_context();
@@ -181,7 +197,10 @@ float RasterizerGPU::get_binning_ratio(size_t count)
 	info.domain = Vulkan::BufferDomain::CachedHost;
 	info.size = impl->binning_mask_buffer->get_create_info().size;
 	auto dst_buffer = impl->device.create_buffer(info);
+	info.size = impl->binning_mask_buffer_coarse->get_create_info().size;
+	auto dst_buffer_coarse = impl->device.create_buffer(info);
 	cmd->copy_buffer(*dst_buffer, *impl->binning_mask_buffer);
+	cmd->copy_buffer(*dst_buffer_coarse, *impl->binning_mask_buffer_coarse);
 	cmd->barrier(VK_PIPELINE_STAGE_TRANSFER_BIT, VK_ACCESS_TRANSFER_WRITE_BIT,
 	             VK_PIPELINE_STAGE_HOST_BIT, VK_ACCESS_HOST_READ_BIT);
 
@@ -191,11 +210,13 @@ float RasterizerGPU::get_binning_ratio(size_t count)
 
 	fence->wait();
 	auto *ptr = static_cast<uint32_t *>(impl->device.map_host_buffer(*dst_buffer, MEMORY_ACCESS_READ_BIT));
+	auto *ptr_coarse = static_cast<uint32_t *>(impl->device.map_host_buffer(*dst_buffer_coarse, MEMORY_ACCESS_READ_BIT));
 
 	unsigned num_tiles_x = (impl->width + 15) / 16;
 	unsigned num_tiles_y = (impl->height + 15) / 16;
 	constexpr unsigned NUM_TILES_X = 2048 / 16;
 	constexpr unsigned TILE_BINNING_STRIDE = 0x4000 / 32;
+	constexpr unsigned TILE_BINNING_STRIDE_COARSE = TILE_BINNING_STRIDE / 32;
 
 	unsigned max_count = 0;
 	unsigned total_count = 0;
@@ -204,9 +225,16 @@ float RasterizerGPU::get_binning_ratio(size_t count)
 		for (unsigned x = 0; x < num_tiles_x; x++)
 		{
 			auto *p = &ptr[TILE_BINNING_STRIDE * (y * NUM_TILES_X + x)];
+			auto *p_coarse = &ptr_coarse[TILE_BINNING_STRIDE_COARSE * (y * NUM_TILES_X + x)];
 			for (size_t i = 0; i < (count + 31) / 32; i++)
 			{
 				uint32_t mask = p[i];
+
+				if (mask != 0)
+					assert((p_coarse[i >> 5] & (1u << (i & 31))) != 0);
+				else
+					assert((p_coarse[i >> 5] & (1u << (i & 31))) == 0);
+
 				unsigned mask_count = __builtin_popcount(mask);
 				total_count += mask_count;
 			}
@@ -215,6 +243,7 @@ float RasterizerGPU::get_binning_ratio(size_t count)
 	}
 
 	impl->device.unmap_host_buffer(*dst_buffer, MEMORY_ACCESS_READ_BIT);
+	impl->device.unmap_host_buffer(*dst_buffer_coarse, MEMORY_ACCESS_READ_BIT);
 	return float(total_count) / float(max_count);
 }
 
