e8cf35cba692b0830ca6141d0fe837aaf604086e
Hans-Kristian Arntzen <maister@archlinux.us>
2019-09-01 13:47:27 +0200

    Refactor out rasterization logic.

---
+++ assets/shaders/rasterizer_helpers.h
@@ -0,0 +1,120 @@
+#ifndef RASTERIZER_HELPERS_H_
+#define RASTERIZER_HELPERS_H_
+
+#include "primitive_setup.h"
+#include "fb_info.h"
+#include "constants.h"
+
+layout(std430, set = 0, binding = PRIMITIVE_SETUP_POS_BUFFER) readonly buffer TriangleSetupPos
+{
+	PrimitiveSetupPos primitives_pos[];
+};
+
+layout(std430, set = 0, binding = PRIMITIVE_SETUP_ATTR_BUFFER) readonly buffer TriangleSetupAttr
+{
+	PrimitiveSetupAttr primitives_attr[];
+};
+
+ivec2 get_interpolation_base(uint primitive_index)
+{
+	return ivec2(primitives_pos[primitive_index].x_a >> 16,
+	             int(primitives_pos[primitive_index].y_lo));
+}
+
+vec3 interpolate_barycentrics(uint primitive_index, int x, int y, ivec2 interpolation_base)
+{
+	float dx = float((x << SUBPIXELS_LOG2) - interpolation_base.x);
+	float dy = float((y << SUBPIXELS_LOG2) - interpolation_base.y);
+	float j = primitives_attr[primitive_index].djdx * dx + primitives_attr[primitive_index].djdy * dy;
+	float k = primitives_attr[primitive_index].dkdx * dx + primitives_attr[primitive_index].dkdy * dy;
+	float i = 1.0 - j - k;
+	return vec3(i, j, k);
+}
+
+vec2 interpolate_uv(uint primitive_index, vec3 bary)
+{
+	float u = dot(primitives_attr[primitive_index].u, bary);
+	float v = dot(primitives_attr[primitive_index].v, bary);
+	float w = dot(primitives_attr[primitive_index].w, bary);
+	w = max(w, 0.00001);
+	return (vec2(u, v) / w) + vec2(ivec2(primitives_attr[primitive_index].uv_offset));
+}
+
+uvec4 interpolate_rgba(uint primitive_index, vec3 bary)
+{
+	vec4 rgba =
+			vec4(uvec4(primitives_attr[primitive_index].color_a)) * bary.x +
+			vec4(uvec4(primitives_attr[primitive_index].color_b)) * bary.y +
+			vec4(uvec4(primitives_attr[primitive_index].color_c)) * bary.z;
+
+	uvec4 urgba = uvec4(clamp(round(rgba), vec4(0.0), vec4(255.0)));
+	return urgba;
+}
+
+ivec2 compute_span_y(uint primitive_index)
+{
+	int span_begin_y = (int(primitives_pos[primitive_index].y_lo) + ((1 << SUBPIXELS_LOG2) - 1)) >> SUBPIXELS_LOG2;
+	int span_end_y = (int(primitives_pos[primitive_index].y_hi) - 1) >> SUBPIXELS_LOG2;
+
+	span_begin_y = max(span_begin_y, fb_info.scissor.y);
+	span_end_y = min(span_end_y, fb_info.scissor.y + fb_info.scissor.w - 1);
+
+	return ivec2(span_begin_y, span_end_y);
+}
+
+ivec2 compute_span_x(uint primitive_index, int y)
+{
+	int y_sub = y << SUBPIXELS_LOG2;
+	int x_a = primitives_pos[primitive_index].x_a +
+	          primitives_pos[primitive_index].dxdy_a * (y_sub - int(primitives_pos[primitive_index].y_lo));
+	int x_b = primitives_pos[primitive_index].x_b +
+	          primitives_pos[primitive_index].dxdy_b * (y_sub - int(primitives_pos[primitive_index].y_lo));
+	int x_c = primitives_pos[primitive_index].x_c +
+	          primitives_pos[primitive_index].dxdy_c * (y_sub - int(primitives_pos[primitive_index].y_mid));
+
+	// The secondary span edge is split into two edges.
+	bool select_hi = y_sub >= int(primitives_pos[primitive_index].y_mid);
+	int primary_x = x_a;
+	int secondary_x = select_hi ? x_c : x_b;
+
+	int start_x, end_x;
+
+	if ((int(primitives_pos[primitive_index].flags) & PRIMITIVE_RIGHT_MAJOR_BIT) != 0)
+	{
+		start_x = (secondary_x + RASTER_ROUNDING) >> (16 + SUBPIXELS_LOG2);
+		end_x = (primary_x - 1) >> (16 + SUBPIXELS_LOG2);
+	}
+	else
+	{
+		start_x = (primary_x + RASTER_ROUNDING) >> (16 + SUBPIXELS_LOG2);
+		end_x = (secondary_x - 1) >> (16 + SUBPIXELS_LOG2);
+	}
+
+	start_x = max(start_x, fb_info.scissor.x);
+	end_x = min(end_x, fb_info.scissor.x + fb_info.scissor.z - 1);
+	return ivec2(start_x, end_x);
+}
+
+bool test_coverage_single(uint primitive_index, int x, int y)
+{
+	ivec2 span_x = compute_span_x(primitive_index, y);
+	ivec2 span_y = compute_span_y(primitive_index);
+
+	bvec4 tests = bvec4(
+			x >= span_x.x, x <= span_x.y,
+			y >= span_y.x, y <= span_y.y);
+	return all(tests);
+}
+
+uint interpolate_z(uint primitive_index, int x, int y, ivec2 interpolation_base)
+{
+	ivec2 d = (ivec2(x, y) << SUBPIXELS_LOG2) - interpolation_base;
+
+	float fz = primitives_attr[primitive_index].z +
+	           primitives_attr[primitive_index].dzdx * d.x +
+	           primitives_attr[primitive_index].dzdy * d.y;
+	uint z = uint(clamp(round(float(0xffff) * fz), 0.0, float(0xffff)));
+	return z;
+}
+
+#endif
